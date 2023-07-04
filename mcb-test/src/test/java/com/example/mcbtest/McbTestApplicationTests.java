7aeac2838e631c4cf2bcf5476833e1eb857d8c42
chunbo.ma <mcbrssq@163.com>
2023-07-04 10:37:04 +0800

    add mcb-test

---
+++ mcb-test/src/test/java/com/example/mcbtest/McbTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mcbtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McbTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
