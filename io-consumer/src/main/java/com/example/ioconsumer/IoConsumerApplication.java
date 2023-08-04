04f4c4c09842bc2eae6cce96171fc849a54ba895
chunbo.ma.o <mcbrssq@163.com>
2023-08-04 11:20:30 +0800

    feat io consumer

---
+++ io-consumer/src/main/java/com/example/ioconsumer/IoConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ioconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IoConsumerApplication.class, args);
+	}
+
+}
---
+++ io-consumer/src/test/java/com/example/ioconsumer/IoConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ioconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
