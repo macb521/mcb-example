5eb1017ce5a4bd446fb8bb74f6765596bc3756ce
chunbo.ma.o <mcbrssq@163.com>
2023-08-04 11:38:35 +0800

    feat add io:consumer,producer

---
+++ io-producer/src/main/java/com/example/ioproducer/IoProducerApplication.java
@@ -0,0 +1,16 @@
+package com.example.ioproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chunbo.ma.o
+ */
+@SpringBootApplication
+public class IoProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IoProducerApplication.class, args);
+	}
+
+}
---
+++ io-producer/src/test/java/com/example/ioproducer/IoProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ioproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
