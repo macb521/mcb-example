10deff4ab4378860441211b7c3d43e47da25dd8c
chunbo.ma.o <mcbrssq@163.com>
2023-08-08 15:12:15 +0800

    feat easyExcel--Converter

--- mcb-test/src/main/java/com/example/test/excel/TestModel.java
+++ mcb-test/src/main/java/com/example/test/excel/TestModel.java
@@ -2,7 +2,6 @@ package com.example.test.excel;
 
 import com.alibaba.excel.annotation.ExcelIgnore;
 import com.alibaba.excel.annotation.ExcelProperty;
-import com.alibaba.excel.annotation.format.DateTimeFormat;
 import lombok.Data;
 
 import java.util.Date;
@@ -14,16 +13,27 @@ import java.util.Date;
 @Data
 public class TestModel {
     @ExcelProperty(index = 0)
-    private String transactionNo;
+    private String test1;
     @ExcelProperty(index = 1)
-    @DateTimeFormat("yyyy/MM/dd HH:mm")
-    private Date stopTime;
+    private String test2;
 
     @ExcelProperty(index = 2)
-    private String commodityName;
+    private String test3;
 
     @ExcelProperty(index = 3)
-    private Date testTime;
+    private String test4;
+
+    @ExcelProperty(index = 4)
+    private Date test5;
+
+    @ExcelProperty(index = 5)
+    private Date test6;
+
+    @ExcelProperty(index = 6)
+    private Date test7;
+
+    @ExcelProperty(index = 7)
+    private Date test8;
     @ExcelIgnore
     private String aaName;
 }
