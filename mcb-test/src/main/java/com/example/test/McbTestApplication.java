1b87d617ae8a0ac58edb88f80db9053acd30c1da
chunbo.ma.o <mcbrssq@163.com>
2023-10-07 16:38:01 +0800

    update test4

--- mcb-test/src/main/java/com/example/test/McbTestApplication.java
+++ mcb-test/src/main/java/com/example/test/McbTestApplication.java
@@ -12,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class McbTestApplication {
 
     public static void main(String[] args) {
+        //test111
         SpringApplication.run(McbTestApplication.class, args);
     }
 
